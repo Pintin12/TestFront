46b86951357a18982e4b4ee01784b183c9c3ae82
Lean <leandro.r@smartwaystudio.com>
2023-08-10 09:10:39 +0200

    Migracion a Pinia

--- src/api/itemApi.ts
+++ src/api/itemApi.ts
@@ -1,10 +1,10 @@
 import axios, { AxiosInstance } from "axios";
 
-const apiClient: AxiosInstance = axios.create({
+const itemApi: AxiosInstance = axios.create({
   baseURL: "http://localhost:5063/api/Items/",
   headers: {
     "Content-type": "application/json",
   },
 });
 
-export default apiClient;
+export default itemApi;
--- src/types/dtos/UpdateDtos.ts
+++
@@ -1,4 +0,0 @@
-export interface UpdateDto {
-    id: number;
-    itemState: boolean;
-  }
